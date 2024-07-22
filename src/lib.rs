a5da793b8834d4c2c4d7c593bb9a768ad60466a8
Miguel Albernaz <albernazmiguel@gmail.com>
2024-07-23 00:53:26 +0100

    make lighting settings public

--- src/lib.rs
+++ src/lib.rs
@@ -10,5 +10,5 @@ mod utils;
 pub mod prelude {
     pub use crate::components::*;
     pub use crate::plugin::Lighting2dPlugin;
-    pub use crate::resources::AmbientLight2d;
+    pub use crate::resources::{AmbientLight2d, Lighting2dSettings};
 }
